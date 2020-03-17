eab049eeb541fb2170e59c831e1ecd45c4ea7de8
Benjamin W <wulfert.benjamin@googlemail.com>
2020-03-17 19:04:05 +0100

    - added fallback keybinds

--- Data/Scripts/Startup/arc_main.lua
+++ Data/Scripts/Startup/arc_main.lua
@@ -28,9 +28,20 @@ Script.ReloadScript("Scripts/Manager/arc_ConstructionController.lua")
 -- load the resource-system, basic
 Script.ReloadScript("Scripts/Manager/arc_ResourcesController.lua")
 
+
+-- keybindings as fallback,
+-- these will be overwritten if keybinds.cfg is correctly set and loaded
+System.ExecuteCommand("bind mwheel_up bIndexInc")           -- select the next construction
+System.ExecuteCommand("bind mwheel_down bIndexDec")         -- select the previous construction
+System.ExecuteCommand("bind 'v' architect_spawn")           -- create the current construction
+System.ExecuteCommand("bind 'g' deleteRayCastEntityHit")    -- delete the currently facing entity (the thing you looking at)
+System.ExecuteCommand("bind 'p' detectEntity")              -- debug: print information about currently facing entity
+System.ExecuteCommand("bind f9 architect_recompile")        -- debug: reload the mods sources
+
 -- load external key bindings
-System.ExecuteCommand('exec Mods/architect/keybinds.cfg')
-System.ExecuteCommand('exec Mods/architect_release/keybinds.cfg')
+-- these file must be stored under:
+-- <path-to-steam-folder>\steamapps\common\KingdomComeDeliverance\mods\architect\keybinds.cfg
+System.ExecuteCommand('exec keybinds.cfg')
 
 -- if this gets printed the mod should work :)
 System.LogAlways("architect " .. architect_version .. " - initialized!")
